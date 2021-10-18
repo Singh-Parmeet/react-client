f302a5862625f4e1b5d55bb579e9f74b77cd20d2
Singh-Parmeet <parmeet.singh@successive.tech>
2021-10-18 22:55:20 +0530

    made change as per feedback

--- src/components/TextField/TextField.jsx
+++ src/components/TextField/TextField.jsx
@@ -4,15 +4,15 @@ import style from './style';
 
 const TextField = (props) => {
   const {
-    placeholder, labels, active, error, error2,
+    placeholder, labels, active, error,
   } = props;
   const errorStyle = error ? style.errorInput : {};
-  const errorStyle2 = error2 ? style.errorInput1 : {};
+  // const errorStyle2 = error2 ? style.errorInput1 : {};
   return (
     <>
       <div>
         <h3>{labels}</h3>
-        <input style={{ ...style.base, ...errorStyle, ...errorStyle2 }} type="text" value={placeholder} disabled={active} />
+        <input style={{ ...style.base, ...errorStyle }} type="text" value={placeholder} disabled={active} />
         {
           error && (
             <div style={style.error}>{error}</div>
@@ -27,7 +27,7 @@ TextField.propTypes = {
   placeholder: PropTypes.string.isRequired,
   labels: PropTypes.string.isRequired,
   active: PropTypes.string.isRequired,
-  error2: PropTypes.string.isRequired,
+  // error2: PropTypes.string.isRequired,
   error: PropTypes.string.isRequired,
 };
 
